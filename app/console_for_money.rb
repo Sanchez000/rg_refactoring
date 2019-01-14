1e1698650f54814bf3308ec880e7aebc71a44e14
Sanchez <artemzub000@gmail.com>
2019-01-14 08:38:55 +0000

    rebuild put_money and send_money

--- app/console_for_money.rb
+++ app/console_for_money.rb
@@ -1,19 +1,15 @@
 module ConsoleForMoney
   def withdraw_money
-    output(:choose_card_withdrawing)
-    return output(:no_cards) unless @current_account.cards.any?
-
-    list_number = select_card(@current_account.cards)
-    return unless list_number
-
-    return output(:wrong_number) unless list_number.between?(0, @current_account.cards.length)
-
-    current_card = @current_account.cards[list_number - 1]
-    withdraw_amount(current_card)
+    puts I18n.t(:choose_card, operation: 'withdrawing')
+    processing('withdrawing')
   end
   
   def put_money
-    output(:choose_card)
+    puts I18n.t(:choose_card, operation: 'putting')
+    processing('putting')
+  end
+  
+  def processing(operation)
     return output(:no_cards) unless @current_account.cards.any?
 
     list_number = select_card(@current_account.cards)
@@ -22,43 +18,28 @@ module ConsoleForMoney
     return output(:wrong_number) unless list_number.between?(0, @current_account.cards.length)
 
     current_card = @current_account.cards[list_number - 1]
-    put_amount(current_card)
-  end
-
-  def withdraw_amount(current_card)
-    transaction = Transaction.new(current_card)
-    run_withdraw(transaction)
-    @current_account.save
-    puts I18n.t(:payment_result, amount: transaction.amount,
-                                 number: current_card.number,
-                                 balance: current_card.balance,
-                                 tax_amount: transaction.tax_amount)
-  end
-
-  def put_amount(current_card)
-    transaction = Transaction.new(current_card)
-    run_put(transaction)
-    @current_account.save
-    puts I18n.t(:payment_result, amount: transaction.amount,
-                                 number: current_card.number,
-                                 balance: current_card.balance,
-                                 tax_amount: transaction.tax_amount)
-  end
-
-  def run_put(transaction)
-    loop do
-      puts I18n.t(:input_amount, operation: 'put on your card')
-      transaction.put_money(gets.chomp)
-      break if transaction.errors.empty?
-
-      transaction.errors.each { |error| puts error }
-    end
+    initialize_transaction(current_card, operation)
   end
   
-  def run_withdraw(transaction)
+  def initialize_transaction(current_card, operation)
+    transaction = Transaction.new(current_card)
+    run_transaction(transaction, operation)
+    @current_account.save
+    puts I18n.t(:payment_result, amount: transaction.amount,
+                                 number: current_card.number,
+                                 balance: current_card.balance,
+                                 tax_amount: transaction.tax_amount)
+  end
+  
+  def run_transaction(transaction, operation)
     loop do
-      puts I18n.t(:input_amount, operation: 'withdraw')
-      transaction.withdraw_money(gets.chomp)
+      if operation == 'putting'
+        puts I18n.t(:input_amount, operation: 'put on your card')
+        transaction.put_money(gets.chomp)
+      else
+        puts I18n.t(:input_amount, operation: 'withdraw')
+        transaction.withdraw_money(gets.chomp)
+      end
       break if transaction.errors.empty?
 
       transaction.errors.each { |error| puts error }
